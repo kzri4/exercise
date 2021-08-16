51dce61e2d725f99a5e9978885fc1204ddb8822f
kzri4 <kazu4126ab@gmail.com>
2021-08-16 10:55:22 +0900

    配列と連想配列の課題完了

---
+++ array_1.php
@@ -0,0 +1,7 @@
+<?php
+
+$countries = ['Japan', 'America', 'China'];
+
+echo $countries[0] . "\n";
+echo $countries[1] . "\n";
+echo $countries[2] . "\n";
---
+++ array_3.php
@@ -0,0 +1,11 @@
+<?php
+
+$nums = [[1,2], [3, 4], [5,6,7]];
+
+echo $nums[1][1] . "\n";
+echo $nums[2][2] . "\n";
+echo $nums[2][1] . "\n";
+echo $nums[2][0] . "\n";
+echo $nums[0][0] . "\n";
+echo $nums[1][0] . "\n";
+echo $nums[0][1] . "\n";
